524376b63ae04840b5e5d02058041dda463d10c9
lai2 <informatica@lai2>
2021-02-10 09:51:55 +0100

    --

--- target/JavaHTTPServerDatabase.sql
+++ target/JavaHTTPServerDatabase.sql
@@ -1,22 +1,22 @@
-DROP DATABASE IF EXISTS HTTPServer;
-CREATE DATABASE HTTPServer;
+DROP DATABASE IF EXISTS JavaHTTPServer;
+CREATE DATABASE JavaHTTPServer;
 
-USE HTTPServer;
+USE JavaHTTPServer;
 
 CREATE TABLE Persone(
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    nome varchar(45),
-    cognome varchar(45)
+	nome varchar(45),
+	cognome varchar(45)
 );
 
 INSERT INTO Persone (nome, cognome)
 VALUES ("Mario", "Rossi"),
-	   ("Giovanni", "Verdi"),
-       ("Luigi", "Bianchi"),
-       ("Marco", "Gialli"),
-       ("Giorgio", "Violi"),
-       ("Lorenzo", "Arancioni"),
-       ("Luciano", "Blu"),
-       ("Francesco", "Rosi"),
-       ("Tommaso", "Marroni"),
-       ("Filippo", "Neri");
+	("Giovanni", "Verdi"),
+	("Luigi", "Bianchi"),
+	("Marco", "Gialli"),
+	("Giorgio", "Violi"),
+	("Lorenzo", "Arancioni"),
+	("Luciano", "Blu"),
+	("Francesco", "Rosi"),
+	("Tommaso", "Marroni"),
+	("Filippo", "Neri");
